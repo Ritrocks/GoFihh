a676f07ebec45bcf70394a5e9f6b63b5d8d893b0
rbhadury <rbhadury@risd.edu>
2026-08-29 18:06:55 -0400

    working build with main menu

---
+++ Assets/scripts/StartGameLoader.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartGameLoader : MonoBehaviour
+{
+    [SerializeField] private String sceneToLoad;
+    /* [SerializeField] private bool loadOnStart = true;
+
+    private void Start()
+    {
+        if (loadOnStart)
+        {
+            LoadSelectedScene();
+        }
+    } */
+
+    public void LoadSelectedScene()
+    {
+        if (sceneToLoad == null)
+        {
+            Debug.LogWarning("StartGameLoader: No scene assigned. Select a scene in the Inspector.");
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name == sceneToLoad)
+        {
+            return;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
